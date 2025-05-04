9ad9d056eab8deabbf896d77b45b4d821429793d
IKRAMUL HAQUE HRIDOY <hridoy.ikramul@gmail.com>
2025-05-04 17:40:27 +0600

    week 2

---
+++ XPCS/Week 2 | Topicwise | STL - 1/A - Make it White.cpp
@@ -0,0 +1,43 @@
+// https://codeforces.com/problemset/problem/1927/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int l, i_c, e_c;
+        cin >> l;
+        string s;
+        cin >> s;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'B')
+            {
+                i_c = i;
+                break;
+            }
+        }
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            if (s[i] == 'B')
+            {
+                e_c = i;
+                break;
+            }
+        }
+        // cout << i_c << " " << e_c << endl;
+        cout << abs(i_c - e_c) + 1 << endl;
+    }
+
+    return 0;
+}
+
+//p - 7 5 3 1 -16 
+///r- 6 4 2 - 12 
---
+++ XPCS/Week 2 | Topicwise | STL - 1/C - Indian Summer.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/problemset/problem/44/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
